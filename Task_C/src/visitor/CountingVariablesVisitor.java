32d27ed373197b8bea533afb4932567985f4cbcb
Max Lund <maxlun@gmail.com>
2018-09-16 15:46:08 +0200

    add working count of variables

--- Task_C/src/visitor/CountingVariablesVisitor.java
+++ Task_C/src/visitor/CountingVariablesVisitor.java
@@ -4,39 +4,50 @@ public class CountingVariablesVisitor extends Visitor {
 
 	int count;
 
+	public CountingVariablesVisitor() {
+		this.count = 0;
+	}
 	
 	public int getCount() {
 		return count;
 	}
-
 
 	@Override
 	public void visit(Number exp) {
 		// TODO Auto-generated method stub
 		
 	}
-
-
+	
 	@Override
 	public void visit(Variable exp) {
 		count++;
 	}
 
-
 	@Override
 	public void visit(Sum exp) {
-		// TODO Auto-generated method stub
+		// create two new CountingVariablesVisitor to count the number of variables in left and right operands
+		CountingVariablesVisitor cv1 = new CountingVariablesVisitor();
+		CountingVariablesVisitor cv2 = new CountingVariablesVisitor();
 		
+		// accept (call visit()) with cv1 and cv2
+		exp.leftOperand.accept(cv1);
+		exp.rightOperand.accept(cv2);
+		
+		// add the counts
+		this.count += cv1.getCount() + cv2.getCount();
 	}
 
 
 	@Override
 	public void visit(Minus exp) {
-		// TODO Auto-generated method stub
+		// same as for Sum
+		CountingVariablesVisitor cv1 = new CountingVariablesVisitor();
+		CountingVariablesVisitor cv2 = new CountingVariablesVisitor();
+		
+		exp.leftOperand.accept(cv1);
+		exp.rightOperand.accept(cv2);
+		
+		this.count += cv1.getCount() + cv2.getCount();
 		
 	}
-
-
-	
-
 }
